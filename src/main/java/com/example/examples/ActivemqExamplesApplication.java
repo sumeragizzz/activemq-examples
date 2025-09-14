3b3f057f4c7b58dc9e6a22c2015681b27e59d5a2
sumeragizzz <developer@sumeragizzz.dev>
2025-09-14 15:03:23 +0900

    Create project by Spring Initializr

---
+++ src/main/java/com/example/examples/ActivemqExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqExamplesApplication.class, args);
+	}
+
+}
