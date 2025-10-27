5251eaa16ef2addc7919828b3c00f8dd4bf90564
sumeragizzz <developer@sumeragizzz.dev>
2025-10-28 08:44:55 +0900

    Implement consumer

--- src/main/java/com/example/examples/ActivemqExamplesApplication.java
+++ src/main/java/com/example/examples/ActivemqExamplesApplication.java
@@ -2,8 +2,10 @@ package com.example.examples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class ActivemqExamplesApplication {
 
 	public static void main(String[] args) {
